64fad70288a3b02352b1cd240f7acece92524ea6
Beejayofgod <adexbolaji100@gmail.com>
2025-12-09 19:58:27 +0100

    Add tours page

---
+++ app/dashboard/tours/page.tsx
@@ -0,0 +1,12 @@
+const ToursPage = () => {
+    return (
+        <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+            voluptatibus veritatis maxime expedita sapiente eos voluptates,
+            culpa similique repellendus odit natus nam beatae. Tempora odio quae
+            cupiditate delectus iure explicabo.
+        </h1>
+    );
+};
+
+export default ToursPage;
